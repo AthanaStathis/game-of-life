8efdc0da20c2a45261964973e03a96f53a0ada84
Athanasios <athana.stathis@gmail.com>
2021-11-15 15:09:34 +0100

    Added one more test for cellPointLiesInLattice(). This time I check that a number of points do not lie within the lattice, as expected. Test-status: PASSED

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -62,4 +62,18 @@ public class BoardTest {
         board.initializeLattice(testLatticeZero);
         assertTrue(board.cellPointLiesInLattice(x, y));
     }
+
+    @ParameterizedTest(name = "Point:[{0},{1}] does not belong to 3x3 matrix")
+    @CsvSource({
+            "-1,-1", "-1,0", "-1,1","-1,2", "-1,-3",
+            "0,-1",                           "0,3",
+            "1,-1",                         "-1,-3",
+            "2,-1",                         "-1,-3",
+            "3,-1",  "3,0",  "3,1",  "3,2",  "3,-3"
+    })
+    void method_cellPointLiesInLattice_returns_false_when_coordinates_are_not_valid_lattice_points(int x, int y) {
+        Board board = new Board();
+        board.initializeLattice(testLatticeZero);
+        assertFalse(board.cellPointLiesInLattice(x, y));
+    }
 }
