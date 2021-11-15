90692bd0df4203435a782e5b5fe0e9cd77735a83
Athanasios <athana.stathis@gmail.com>
2021-11-15 15:01:03 +0100

    Cretaed a second test for initializeLattice(). The purpose is to see that the method can reliably delivers the lattice-form being required

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -38,4 +38,16 @@ public class BoardTest {
         board.initializeLattice(testLatticeZero);
         assertEquals(board.lattice[row][col].getState(), expectedState);
     }
+
+    @ParameterizedTest(name = "Cell:[{0},{1}], Expected-State={2}")
+    @CsvSource({
+            "0,0,.", "0,1,0", "0,2,.",
+            "1,0,.", "1,1,0", "1,2,.",
+            "2,0,.", "2,1,0", "2,2,."
+    })
+    void initialize_board_according_to_first_generation_lattice(int row, int col, String expectedState) {
+        Board board = new Board();
+        board.initializeLattice(testLatticeOne);
+        assertEquals(board.lattice[row][col].getState(), expectedState);
+    }
 }
