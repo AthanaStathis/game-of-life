485843e91f0ce36fd33acf345ecb234004824d88
Athanasios <athana.stathis@gmail.com>
2021-11-15 14:44:46 +0100

    Created method initializeLattice() in class Board. Status of test for initializing the lattice of a board: PASSED

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,16 @@
 public class Board {
     public Cell[][] lattice;
+    int latticeHeight = 0;
+    int latticeWidth = 0;
+
+    public Board() {
+
+    }
+
+    public void initializeLattice(Cell[][] lattice) {
+        this.lattice = lattice;
+        this.latticeHeight = lattice.length;
+        this.latticeWidth = lattice[0].length;
+    }
+
 }
